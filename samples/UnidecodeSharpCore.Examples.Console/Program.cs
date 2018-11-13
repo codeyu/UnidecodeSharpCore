9bb9d5a694a983b36ac488d0458b8ce0bf41c1c0
codeyu <codeyu@gmail.com>
2018-11-13 16:29:01 +0800

    fix samples

--- samples/UnidecodeSharpCore.Examples.Console/Program.cs
+++ samples/UnidecodeSharpCore.Examples.Console/Program.cs
@@ -6,7 +6,7 @@ namespace UnidecodeSharpCore.Examples.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
